6deb0f761bc7d8c4d4c429669e6d39f491dae682
lauronbraydenlareez-cmyk <lauronbraydenlareez@gmail.com>
2025-08-23 11:23:21 +0000

    Seatwork 5 output

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
+public class Main {
+Ppublic static void main(String[] args) {
+System.out.println("SEATWORKFIVE");
 
+int countdown = 3;
+while (countdown > 0 ) {
+    System.out.println(countdown);
+    countdown--;
+}
+
+System.out.println("Happy New Year!!");
+}
+}
